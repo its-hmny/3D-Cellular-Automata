ad98cf9c549cab124196e0f069e320ea78d00b66
Enea Guidi <enea.guidi@botika.ai>
2022-06-10 11:54:32 +0200

    feat(SettingsForm): Added more UI components & functionalities

--- components/SettingsForm.tsx
+++ components/SettingsForm.tsx
@@ -1,8 +1,25 @@
-import { Grid, Input, Radio, Spacer, Text } from '@nextui-org/react';
+import { Button, Grid, Input, Radio, Spacer, Text } from '@nextui-org/react';
+import { Checkmark, Close, Refresh, UploadOption } from 'grommet-icons';
 
 const SettingsForm = () => {
   return (
     <Grid.Container gap={2}>
+      <Grid xs={6} justify="center">
+        <Button color="primary" flat auto>
+          Generate random seed
+        </Button>
+      </Grid>
+      <Grid xs={6} justify="center">
+        <Button color="primary" flat auto>
+          Import seed from file
+        </Button>
+      </Grid>
+
+      {/* Spacing */}
+      <Grid xs={12}>
+        <Spacer y={1} />
+      </Grid>
+
       <Grid xs={12}>
         <Radio.Group label="Options" defaultValue="conway">
           <Radio
@@ -22,6 +39,27 @@ const SettingsForm = () => {
         </Radio.Group>
       </Grid>
 
+      {/* Spacing */}
+      <Grid xs={12}>
+        <Spacer y={1} />
+      </Grid>
+
+      <Grid xs={6}>
+        <Input
+          type="number"
+          color="primary"
+          label="Matrix size"
+          helperText="The number of cell per side in the matrix"
+        />
+      </Grid>
+      <Grid xs={6}>
+        <Input
+          type="number"
+          color="primary"
+          label="Life states"
+          helperText="The max age reachable by any given cell"
+        />
+      </Grid>
       <Grid xs={6}>
         <Input
           type="number"
@@ -37,6 +75,22 @@ const SettingsForm = () => {
           label="Survive threshold"
           helperText="Number of alive neighbor for a cell to survive"
         />
+      </Grid>
+
+      {/* Spacing */}
+      <Grid xs={12}>
+        <Spacer y={7.5} />
+      </Grid>
+
+      <Grid xs={6}>
+        <Button color="error" flat icon={<Close />}>
+          Discard
+        </Button>
+      </Grid>
+      <Grid xs={6}>
+        <Button color="success" flat icon={<Checkmark />}>
+          Save
+        </Button>
       </Grid>
     </Grid.Container>
   );
