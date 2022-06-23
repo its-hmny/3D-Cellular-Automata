5461c554252ac456312d7de5368989c14d5e7fb2
Enea Guidi <enea.guidi@botika.ai>
2022-06-23 14:04:29 +0200

    refactor(sass): Removed sass files and dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { createTheme, NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
 import { SimulationProvider } from '../context/Simulation';
-import '../styles/style.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const theme = createTheme({ type: 'dark', theme: { colors: { primary: 'orange' } } });
