d423a66b151dcf7b98d045d238970320b941b809
its-hmny <its-hmny@users.noreply.github.com>
2022-07-05 22:25:15 +0000

    deploy: e940cc35ec920b873896d756539f4dedddf9e77b

--- README.md
+++
@@ -1,2 +0,0 @@
-# 3D-Cellular-Automata
-A Three.js webapp to simulate cellular automata in 3D
---
+++ _next/static/ZCGVjFKoagwFI9b-ycHHL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-ab243316717ca7b2.js","static/chunks/678-ab91954143893862.js","static/chunks/pages/index-52be4aeadc06e630.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
