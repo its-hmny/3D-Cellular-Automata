fde3e30bdd82b513a19d79a5d91e05d9063a6340
Enea Guidi <guidienea@gmail.com>
2022-07-06 00:45:49 +0200

    fix(config): Fixed next.config.js to account for production env
    The current commit adds a "dynamic" next.config.js file based on the current env.
    Since the site is deployed to GitHub Pages as a project page at the following url:
    {username}.github.io/{projectname} but Next.js expects  the assetts to be served
    at the root location: {username}.github.io.
    Since this is not possible in GitHub Pages, this commit fixes the issue by hardcoding
    the basePath and assetPath variable in the configuration file.
    
    Credits: https://wallis.dev/blog/deploying-a-next-js-app-to-github-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  // The path at which the assets and other files (such as html, css, ...)
+  // will be served when executing in production environment (GitHub pages)
+  basePath: isProduction ? '/3D-Cellular-Automata' : '',
+  assetPrefix: isProduction ? '/3D-Cellular-Automata' : '',
+};
+
+module.exports = nextConfig;
