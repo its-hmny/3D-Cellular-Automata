9dfb0ba6ddea7625e4a20993c077f6b607595dd6
Enea Guidi <guidienea@gmail.com>
2022-09-01 23:49:12 +0200

    style(all): Adds more integrated 'selection' color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,10 @@ import { SimulationProvider } from '../context/Simulation';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Custom NextUI app theme (dark + orange as primary color)
-  const theme = createTheme({ type: 'dark', theme: { colors: { primary: 'orange' } } });
+  const theme = createTheme({
+    type: 'dark',
+    theme: { colors: { primary: 'orange', selection: '#704705' } },
+  });
 
   return (
     <NextUIProvider theme={theme}>
